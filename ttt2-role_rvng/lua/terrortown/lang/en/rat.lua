1610bb4f58758c0494cc2e17198afd8f4b5ee7f2
milkwxter <ethan.r.bates2003@gmail.com>
2024-07-01 20:43:32 -0500

    Role should work, initial commit

---
+++ ttt2-role_rvng/lua/terrortown/lang/en/rat.lua
@@ -0,0 +1,9 @@
+local L = LANG.GetLanguageTableReference("en")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[REVENGER.name] = "Revenger"
+L["info_popup_" .. REVENGER.name] = [[You are the Revenger! If someone kills your lover, you must track them down.]]
+L["body_found_" .. REVENGER.abbr] = "They were a Revenger."
+L["search_role_" .. REVENGER.abbr] = "This person was a Revenger!"
+L["target_" .. REVENGER.name] = "Revenger"
+L["ttt2_desc_" .. REVENGER.name] = [[The Revenger must protect their lover. If they fail to do so, the killer is revealed to the Revenger.]]
